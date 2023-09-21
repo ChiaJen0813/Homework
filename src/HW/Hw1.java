148c9b7e6a111c0ff9588ac6f817262085198923
Chia Jen <abc0956000813@gmail.com>
2023-09-21 16:26:52 +0800

    Finish Hw1 & Hw2

--- src/HW/Hw1.java
+++ src/HW/Hw1.java
@@ -1,56 +1,54 @@
 package HW;
 
 public class Hw1 {
+//	請設計一隻Java程式，計算12，6這兩個數值的和與積
 	public static void main(String[] args) {
 		int x = 12, y = 6;
 		int t1 = x + y;
 		int t2 = x * y;
-
 		System.out.println(t1);
 		System.out.println(t2);
 
 		System.out.println("===================");
 
+//		請設計一隻Java程式，計算200顆蛋共是幾打幾顆？ (一打為12顆)
 		int egg = 200, dozen = 12;
 		int h1 = egg / dozen;
 		System.out.println(h1);
 
 		System.out.println("===================");
 
-//		int time = 256559, sec = 1, min = 60 * sec, hour = 60 * min, day = 24 * hour;
-//		int D = time / day;
-//		int H = time / hour;
-//		int M = time / min;
-//		int S = time / sec;
-//		System.out.println(D);
-//		System.out.println(H);
-//		System.out.println(M);
-//		System.out.println(S);
-
+//		請由程式算出256559秒為多少天、多少小時、多少分與多少秒
 		int num = 256559, sec = 1, min = sec * 60, hour = min * 60, day = hour * 24;
 		System.out.printf("%d秒是%d天%d時%d分%d秒%n", num, num / day, (num % day) / hour, ((num % day) % hour) / min,
 				(((num % day) % hour) % min) / sec);
 
 		System.out.println("===================");
-
-		final double Pi = 3.1415;
+		
+//		請定義一個常數為3.1415(圓周率)，並計算半徑為5的圓面積與圓周長
+		double Pi = 3.1415;
 		int r = 5;
 		double 圓面積 = r * 2 * Pi;
-		System.out.println(圓面積);
+		System.out.println("圓面積 ="+ 圓面積);
 		double 圓周長 = Pi * Math.pow(r, 2);
-		System.out.println(圓周長);
+		System.out.println("圓周長 ="+ 圓周長);
 
 		System.out.println("===================");
 
+//		某人在銀行存入150萬，銀行利率為2%，如果每年利息都繼續存入銀行，請用程式計算10年後，本
+//		金加利息共有多少錢 (用複利計算，公式請自行google)
 		int money = 1_500_000;
 		double rate = 1.02;
 		System.out.println(money * Math.pow(rate, 10)); // 1.02十次方*本金
 
 		System.out.println("===================");
 
+//		請寫一隻程式，利用System.out.println()印出以下三個運算式結果：
 		System.out.println(5 + 5); // 就是5+5=10
-		System.out.println(5 + '5'); //
+		System.out.println(5 + '5'); //此為預設整數型別int數值5+型別char5，依Unicode編碼char5代表十六進位值為0x35，換算十進位數值為型別int數值53，所得十進位之和int數值58
 		System.out.println(5 + "5"); // 5加字串5=55
+		
+				
 	}
 
 }
--- src/HW/Hw2.java
+++ src/HW/Hw2.java
@@ -5,18 +5,20 @@ public class Hw2 {
 
 //		請設計一隻Java程式，計算1～1000的偶數和 (2+4+6+8+…+1000)
 		int a = 0;
-		for (int b = 2; b <= 1000; b += 2){
-			a=a+b;}    //a += b;}
+		for (int b = 2; b <= 1000; b += 2) {
+			a += b;
+		}
 		System.out.println(a);
-		
+
 		System.out.println("===================");
 
 //		請設計一隻Java程式，計算1～10的連乘積 (1*2*3*…*10) (用for迴圈)
 		int c = 1;
 		for (int d = 1; d <= 10; d++) {
-			c=c*d;}     //c *= d;}
+			c *= d;
+		}
 		System.out.println(c);
-		
+
 		System.out.println("===================");
 
 //		請設計一隻Java程式，計算1～10的連乘積 (1*2*3*…*10) (用while迴圈)
@@ -27,12 +29,117 @@ public class Hw2 {
 			f++;
 		}
 		System.out.println(e);
-		
+
 		System.out.println("===================");
 
 //		請設計一隻Java程式，輸出結果為以下：
-//		1 4 9 16 25 36 49 64 81 100
-		
-	}
+//		1 4 9 16 25 36 49 64 81 100		
+		for (int g = 1; g <= 10; g++) {
+			int h = (int) Math.pow(g, 2); // Math.pow預設為double
+			System.out.print(h + " ");
+		}
 
+		System.out.println("\n===================");
+
+//		阿文很熱衷大樂透 (1 ～ 49)，但他不喜歡有4的數字，不論是個位數或是十位數。請設計一隻程式，
+//		輸出結果為阿文可以選擇的數字有哪些？總共有幾個？
+//		int num= 0;
+//		int count = 0;
+//		while(num++ < 39)
+//			if (num == 4 || num ==14 || num == 24 || num == 34)
+//			{System.out.print("");}
+//			else {
+//				System.out.print(num + ", ");
+//				count++;
+//			}
+//		System.out.print("總共有 = " + count);
+		int num = 0;
+		for (int z = 1; z <= 49; z++) {
+			if ((z % 10 == 4) || (z / 10) == 4) {
+				System.out.print("");
+			} else {
+				num += 1;
+				System.out.print(z + ", ");
+			}
+		}
+		System.out.println();
+		System.out.print("總共有" + num + "個數字");
+
+		System.out.println("\n===================");
+
+//		• 請設計一隻Java程式，輸出結果為以下：
+//		1 2 3 4 5 6 7 8 9 10
+//		1 2 3 4 5 6 7 8 9
+//		1 2 3 4 5 6 7 8
+//		1 2 3 4 5 6 7
+//		1 2 3 4 5 6 
+//		1 2 3 4 5
+//		1 2 3 4 
+//		1 2 3 
+//		1 2 
+//		1
+
+		for (int m = 10; m >= 0; m--) {
+			for (int n = 1; n <= m; n++) {
+				System.out.print(n + " ");
+			}
+			System.out.println();
+		}
+
+		System.out.println("\n===================");
+
+//		 請設計一隻Java程式，輸出結果為以下：
+//		 A
+//		 BB
+//		 CCC
+//		 DDDD
+//		 EEEEE
+//		 FFFFFF	
+		char word = 65;
+		for (int i = 1; i <= 6; i++) {
+			for (int j = 1; j <= i; j++) {
+				System.out.print(word);
+			}
+			word += 1;
+			System.out.println();
+		}
+		System.out.println("\n===================");
+
+		// 輸出九九乘法表
+//		1.用for迴圈+while迴圈
+
+		int i;
+		for (i = 1; i <= 9; i++) {
+			int j = 0;
+			while (++j <= 9) {
+				System.out.print(i + "*" + j + "=" + i * j + "\t");
+			}
+			System.out.println();
+		}
+		System.out.println("\n===================");
+
+//		2.使用for迴圈+do while迴圈
+		for (int k = 1; k <= 9; k++) {
+			int m = 1;
+			do {
+				System.out.print(k + "*" + m + "=" + k * m + "\t");
+			} while (++m <= 9);
+			System.out.println();
+		}
+		System.out.println("\n===================");
+
+//      3.使用while迴圈+do while迴圈
+
+		int q = 1;
+		while (q <= 9) {
+			int s = 1;
+			do {
+				System.out.print(q + "*" + s + "=" + q * s + "\t");
+				s += 1;
+			} while (s <= 9);
+			System.out.println();
+			q += 1;
+		}
+
+	}
 }
